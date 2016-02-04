9a37c9019a652a9c4ab6d6187c8558b492f61291
aromatt <amatteson3@gmail.com>
2016-02-03 23:32:29 -0500

    Moved current DJ, track title and remaining time down to bottom navbar

--- public/javascripts/resizing.js
+++ public/javascripts/resizing.js
@@ -6,6 +6,26 @@ function refreshSizes(playerLoaded) {
   var mainRowWidth = $(window).width();
   var playerPos = $('#player').offset();
 
+  // Chat input width
+  $('#chat-input').outerWidth($('#chat-tab-content').width());
+  $('#chat-input').css({
+    'margin-right': parseInt($('#jub-col-chat').css('padding-right'), 10) +
+                    parseInt($('#jub-container').css('padding-right'), 10)
+  })
+
+  // DJ banner widths
+  var djBannerWidth =
+    mainRowWidth
+    - $('#chat').outerWidth()
+    - parseInt($('#dj-container').css('padding-left'), 10)
+    - parseInt($('#dj-container').css('padding-right'), 10)
+    - 20;
+  $('#dj-container').innerWidth(djBannerWidth);
+  $('#current-dj-name').innerWidth(
+    $("#queue-panel").innerWidth()
+    - $("#time-left").outerWidth()
+  );
+
   // Chat messages and "who's jubbin" list
   var chatTabsBottom = $('#chat-navtabs').offset().top + $('#chat-navtabs').height();
   var msgsHeight = $('#bottom-nav').position().top - chatTabsBottom - 10;
@@ -17,18 +37,13 @@ function refreshSizes(playerLoaded) {
     $('#jubbin-list').innerHeight(msgsHeight);
   }
 
-  // Chat input width
-  $('#chat-input').outerWidth($('#chat-tab-content').width());
-  $('#chat-input').css({
-    'margin-right': parseInt($('#jub-col-chat').css('padding-right'), 10) +
-                    parseInt($('#jub-container').css('padding-right'), 10)
-  })
-
   // Set queue and playlists height
   $('#queue-panel').innerHeight(msgsHeight - $('#queue-banner').outerHeight());
-  $('#video-queue').innerHeight(msgsHeight
-                                - $('#queue-banner').outerHeight()
-                                - $('#playlist-name-banner').outerHeight());
+  $('#video-queue').innerHeight(
+    msgsHeight
+    - $('#queue-banner').outerHeight()
+    - $('#playlist-name-banner').outerHeight()
+  );
   $('#playlists-panel').innerHeight(msgsHeight - $('#playlists-banner').outerHeight());
   $('#playlists').innerHeight(msgsHeight - $('#playlists-banner').outerHeight());
 }
