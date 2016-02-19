a5ec78cede36ef17f896dbf1ab2eb30ecb10ba7f
aromatt <amatteson3@gmail.com>
2016-02-18 23:35:31 -0500

    Maybe serve favicon

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.locals.pretty = true;
 
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
