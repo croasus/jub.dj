f7a447cbbb6ac5df62587b0228dad37f28c52caa
aromatt <amatteson3@gmail.com>
2016-05-23 21:18:36 -0400

    updates to UI tests

--- test/util.js
+++ test/util.js
@@ -3,15 +3,21 @@ var fs = require ('fs');
 var path = require ('path');
 
 artifact_dir = process.env.JUB_TEST_ARTIFACT_DIR || path.join(__dirname, '../artifacts');
+default_levels = ['ERROR', 'WARNING', 'INFO', 'DEBUG'];
 
 module.exports = {
   DEFAULT_WAIT: 5000,
-  writeLogs: function writeLogs(entries, levels) {
-    if (!levels) { levels = ['ERROR', 'WARNING', 'INFO', 'DEBUG']; }
-    _.each(entries, function(log) {
-      writePath = path.join(artifact_dir, path.basename(__filename) + '.browserlog');
-      data = '[' + log.level + ']' + log.timestamp + ' : ' + log.message + '\n';
-      fs.appendFileSync(writePath, data);
-    });
+  logWriter: function logWriter(test_path, levels) {
+    if (!levels) { levels = default_levels; }
+    return function(entries) {
+      if (!test_path) { throw "No test_path given"; }
+        writePath = path.join(artifact_dir,
+                              path.basename(test_path, '.js') + '.browserlog');
+        fs.unlink(writePath);
+      _.each(entries, function(log) {
+        data = '[' + log.level + ']' + log.timestamp + ' : ' + log.message + '\n';
+        fs.appendFileSync(writePath, data);
+      });
+    }
   },
 }
