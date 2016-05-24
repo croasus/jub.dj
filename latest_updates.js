faac3c1995c2b249a2296917b6d922bf368ba46e
aromatt <amatteson3@gmail.com>
2016-05-23 22:52:04 -0400

    update latest_updates

--- latest_updates.js
+++ latest_updates.js
@@ -1,7 +1,6 @@
 module.exports = {
-  date: '2015-4-18',
+  date: '2015-5-23',
   list: [
-    "Users' mute statuses are displayed in the Who's Jubbin' list.",
-    "You can keep your own mute status private via the chat settings.",
+    "jubbot: locate [user]",
   ],
 }
