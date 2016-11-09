370da93d3c0ab8d1f81eff6cc31942080d000f86
aromatt <amatteson3@gmail.com>
2016-11-09 00:13:21 -0500

    Added mute status assertions to prefs test case

--- test/ui/test_005_prefs.js
+++ test/ui/test_005_prefs.js
@@ -6,17 +6,21 @@ module.exports = {
     browser.resizeWindow(1920, 1080)
       .url('http://localhost:3001/' + config.private_room)
       .waitForElementVisible('#chat-settings-navtab-a', util.DEFAULT_WAIT)
+      .verify.attributeEquals('#enable-mute-updates', 'checked', 'true')
       .expect.element('#show-chat-images').to.not.have.attribute('checked');
     browser
       .click('#chat-settings-navtab-a')
       .waitForElementVisible('#chat-settings-panel', util.DEFAULT_WAIT)
       .click('input[id="show-chat-images"]')
+      .click('input[id="enable-mute-updates"]')
       .verify.attributeEquals('#show-chat-images', 'checked', 'true')
-      .pause(500);
+      .expect.element('#enable-mute-updates').to.not.have.attribute('checked');
     browser
       .refresh()
       .waitForElementVisible('#chat-settings-navtab-a', util.DEFAULT_WAIT)
       .verify.attributeEquals('#show-chat-images', 'checked', 'true')
+      .expect.element('#enable-mute-updates').to.not.have.attribute('checked');
+    browser
       .getLog('browser', util.logWriter(__filename))
       .end();
   },
